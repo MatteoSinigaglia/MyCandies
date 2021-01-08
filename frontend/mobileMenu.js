9fa96f3139232a9832bf73412af984c97c5966be
Matteo Sinigaglia <matteo.sinigaglia99@gmail.com>
2021-01-08 23:01:44 +0100

    creato menu ad hamburger

---
+++ frontend/mobileMenu.js
@@ -0,0 +1,8 @@
+function mobileMenu() {
+    var menu = document.getElementById("navigationMenu");
+    if(menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+};
