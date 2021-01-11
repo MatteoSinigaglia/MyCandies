c13c14747d737f7fefbe30f8d28fe12d4d684b9a
Matteo Sinigaglia <matteo.sinigaglia99@gmail.com>
2021-01-11 22:01:14 +0100

    implementato menu funzioante, supporto per js disattivato

--- frontend/mobileMenu.js
+++ frontend/mobileMenu.js
@@ -1,8 +1,20 @@
+function addStyleSheet() {
+    var head = document.head;
+    var link = document.getElementById("mobileStyleSheet");
+
+    link.href = "../frontend/style_mobile.css";
+
+    head.appendChild(link);
+}
+
 function mobileMenu() {
     var menu = document.getElementById("navigationMenu");
+    var button = document.getElementById("mobileMenu");
     if(menu.style.display === "block") {
         menu.style.display = "none";
+        button.className = "fa fa-bars";
     } else {
         menu.style.display = "block";
+        button.className = "fa fa-close";
     }
 };
