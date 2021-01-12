476998c41895cd2de1e95206038410809d774a12
Matteo Sinigaglia <matteo.sinigaglia99@gmail.com>
2021-01-12 11:26:16 +0100

    aggiunta icon al bottone carrello

--- frontend/mobileMenu.js
+++ frontend/mobileMenu.js
@@ -1,10 +1,13 @@
 function addStyleSheet() {
     var head = document.head;
     var link = document.getElementById("mobileStyleSheet");
-
     link.href = "../frontend/style_mobile.css";
-
     head.appendChild(link);
+    if(window.screen.width < 768) {
+        var carrello = document.getElementById("cartButton");
+        carrello.className = "fa fa-shopping-cart";
+        carrello.innerHTML = "";
+    }
 };
 
 function mobileMenu() {
