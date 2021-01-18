7658d21b7483c499a8637723ddb4e83254db3237
Matteo Sinigaglia <matteo.sinigaglia99@gmail.com>
2021-01-18 23:24:04 +0100

    sistemato js per ridimensionare pagine ottenendo il menu corretto

--- frontend/mobileMenu.js
+++ frontend/mobileMenu.js
@@ -1,10 +1,20 @@
 function loadMenu() {
     if(window.screen.width <= 767) {
         var menuButton = document.getElementById("mobileMenu_hidden");
+        if(!menuButton) {
+            menuButton = document.getElementById("mobileMenu_visible");
+        }
         menuButton.id = "mobileMenu_visible";
         var menu = document.getElementById("navigationMenu");
         menu.style.display = "none";
+    } else {
+        var menu = document.getElementById("navigationMenu");
+        menu.style.display = "block";
     }
+}
+
+window.onresize = function() {
+    loadMenu();
 }
 
 function mobileMenu() {
