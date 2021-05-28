d34faf90ca6d16e3a52bfcb19d10467dda1cc383
Daniel Eduardo Contro <dani.contro2@gmail.com>
2021-05-28 15:03:47 +0200

    Fix header FAQ

--- backend/FAQ.php
+++ backend/FAQ.php
@@ -1,7 +1,19 @@
 <?php
 
+
 require_once '..' . DIRECTORY_SEPARATOR . 'paths_index.php';
+require_once __DIR__.'/classes/MyCandies/Controllers/Authentication.php';
+
+use MyCandies\Controllers\Authentication;
 
 $DOM = file_get_contents('../frontend/FAQ.html');
+
+$auth = new Authentication();
+
+if  ($auth->isLoggedIn()) {
+	$DOM = str_replace('<a_auth_state />', '<a href="logout.php" id="loginButton" class="fa fa-sign-out buttons"><span> Logout</span></a>', $DOM);
+} else {
+	$DOM = str_replace('<a_auth_state />', '<a href="./formCliente.php" id="loginButton" class="fa fa-sign-in buttons"><span> Accedi</span></a>', $DOM);
+}
 
 echo $DOM;
