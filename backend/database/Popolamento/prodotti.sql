330dca5fbd961277b8e559ad24e78f985c9a29bf
MatteoSinigaglia <matteo.sinigaglia99@gmail.com>
2021-06-10 11:48:02 +0200

    cambiate categorie e immagini

--- backend/database/Popolamento/prodotti.sql
+++ backend/database/Popolamento/prodotti.sql
@@ -1,27 +1,27 @@
 DELETE FROM `Products`;
 INSERT INTO `Products` (`id`, `category_id`, `name`, `description`, `price`, `availability`) VALUES 
 
-(1,1,"nome","nootropi",8.99,30),
+(1, 1, "Balsamiche nootropiche", "nootropi", 8.99, 30),
 
-(2,2,"nome","taurina",10,300),
+(2, 2, "Taurina mou", "taurina", 10, 300),
 
-(3,5,"nome","gas ionizzato", 23.00, 450),
+(3, 5, "Cola ionizzate", "gas ionizzato", 23.00, 450),
 
-(4,4,"nome","elio", 150.00, 100),
+(4, 4, "Anguria leggera", "elio", 150.00, 100),
 
-(5,5,"nome","gas ionizzato",98.00,20000),
+(5, 5, "Squali ionizzati", "gas ionizzato", 98.00, 2000),
 
-(6,3,"nome","testosterone", 49.00, 200),
+(6, 3, "Testocola", "testosterone", 549.00, 200),
 
-(7,3,"nome","pietra filosofale",6,300),
+(7, 3, "Orsetti filosofali", "pietra filosofale", 6, 300),
 
-(8,5,"nome","caffeina", 70.00, 5),
+(8, 5, "Liquirizie ripiene di caffeina", "caffeina", 70.00, 5),
 
-(9,5,"nome","caffeina",600,40)
+(9, 2, "Caramelle al caffè gusto caffè", "caffeina", 64.50, 40)
 ;
 
 DELETE FROM `Categories`;
-INSERT INTO `Categories` (`id`, `name`) VALUES (1,'Assorbimento lento'),(2,'Assorbimento rapido'),(3,'Effetto permanente'),(4,'Effetto breve'),(5,'Solubili');
+INSERT INTO `Categories` (`id`, `name`) VALUES (1,'Balsamiche'),(2,'Senza zucchero'),(3,'Gelatine'),(4,'Frutta'),(5,'Gommose');
 
 DELETE FROM `ActivePrinciples`;
 INSERT INTO `ActivePrinciples` (`id`, `name`) VALUES (1,'Taurina'),(2,'Gas ionizzato'),(3,'Testosterone'),(4,'Materia bianca'),(5,'Nootropi'),(6,'Elio'),(7,'Pietra filosofale'),(8,'Caffeina');
@@ -42,7 +42,7 @@ DELETE FROM `ActivePrinciplesSideEffects`;
 INSERT INTO `ActivePrinciplesSideEffects` (`active_principle_id`, `side_effect_id`) VALUES (1,1),(2,5),(3,3),(4,4),(5,2),(6,8),(7,6),(8,7);
 
 DELETE FROM `Images`;
-INSERT INTO `Images` (`id`, `img_path`) VALUES (1,'../img/products/orsetti.jpg'),(2,'../img/products/cola_zuccherate.jpg'),(3,'../img/products/gommose_rosa.jpg'),(4,'../img/products/liquirizia_ripiena.jpg'),(5,'../img/products/perle_colorate.jpg'),(6,'../img/products/rotelle_liquirizia.jpg'),(7,'../img/products/strisce_colorate.jpg'),(8,'../img/products/uova'),(9,'../img/products/zuccherate');
+INSERT INTO `Images` (`id`, `img_path`) VALUES (1,'../img/products/balsamiche.jpg'),(2,'../img/products/mou.jpg'),(3,'../img/products/cola_gommose.jpg'),(4,'../img/products/anguria.jpg'),(5,'../img/products/squali_gommosi.jpg'),(6,'../img/products/cola.jpg'),(7,'../img/products/orsetti.jpg'),(8,'../img/products/liquirizia_ripiene.jpg'),(9,'../img/products/caffe.png');
 
 DELETE FROM `ProductsImages`;
 INSERT INTO `ProductsImages` (`img_id`, `product_id`) VALUES (1,1),(2,2),(3,3),(4,4),(5,5),(6,6),(7,7),(8,8),(9,9);
