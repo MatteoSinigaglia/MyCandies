87c284eb92185c45acb0bc1897e1b6dd1297c602
Daniel Eduardo Contro <dani.contro2@gmail.com>
2021-06-24 19:24:35 +0200

    Fix 404

--- backend/error404.php
+++ backend/error404.php
@@ -1,11 +1,23 @@
 <?php
 
-$htmlPage = file_get_contents(VIEW_PATH . DS . "error404.html");
+require_once '..' . DIRECTORY_SEPARATOR . 'paths.php';
+require_once MYCANDIES_PATH . DS . 'Controllers' . DS . 'Authentication.php';
 
-$htmlPage = str_replace('<dashboard />', ($auth->isAdmin()
+use \MyCandies\Controllers\Authentication;
+
+$DOM = file_get_contents(VIEW_PATH . DS . "error404.html");
+
+$auth = new Authentication();
+
+if  ($auth->isLoggedIn()) {
+    $DOM = str_replace('<a_auth_state />', '<a href="logout.php" id="loginButton" class="fa fa-sign-out buttons"><span xml:lang="en"> Logout</span></a>', $DOM);
+} else
+    $DOM = str_replace('<a_auth_state />', '<a href="./formCliente.php" id="loginButton" class="fa fa-sign-in buttons"><span> Accedi</span></a>', $DOM);
+
+$DOM = str_replace('<dashboard />', ($auth->isAdmin()
     ?
     '<li><a href="../backend/inserisciProdotto.php">Gestione</a></li>'
     :
-    ''), $htmlPage);
+    ''), $DOM);
 
-echo $htmlPage;
+echo $DOM;
